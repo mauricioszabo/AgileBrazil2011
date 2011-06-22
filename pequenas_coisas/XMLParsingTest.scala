25272b43bd4f91fb1aeb56c739404f6302ad1277
Maurício Szabo <mauricio.szabo@gmail.com>
2011-06-21 23:58:27 -0300

    XML Parsing em Scala pronto.

--- pequenas_coisas/XMLParsingTest.scala
+++ pequenas_coisas/XMLParsingTest.scala
@@ -4,24 +4,33 @@ import scala.xml._
 
 class XMLParsingTest extends FlatSpec with ShouldMatchers {
     it should "get the correct number of people on XML" in {
-        (xml \ "person").size should be === (2)
+        (XML.loadString(xml) \ "person").size should be === (2)
     }
 
     it should "get the correct number of people" in {
         xml should have(2).tags("person")
     }
 
-    def xml = <people><person>Ariovaldo Steil</person><person>Ariovaldo Gumercindo</person></people>
+    def xml = """
+    <people>
+        <person>Ariovaldo Steil</person>
+        <person>Ariovaldo Gumercindo</person>
+    </people>
+    """
 
     def have(count: Int) = new TagMatcher(count)
     
     class TagMatcher(count: Int) {
-        def tags(tag: String) = new Matcher[Seq[Node]] {
-            def apply(xml: Seq[Node]) = MatchResult(
-                (xml \ tag).size == count,
-                "expected to have " + count + " <" + tag + "> tags",
-                "expected to not have " + count + " <" + tag + "> tags"
-            )
+        def tags(tag: String) = new Matcher[String] {
+            def apply(xmlString: String) = {
+                val xml = XML.loadString(xmlString)
+                val elements = xml \ tag
+                MatchResult(
+                    elements.size == count,
+                    "expected to have " + count + " <" + tag + "> tags",
+                    "expected to not have " + count + " <" + tag + "> tags"
+                )
+            }
         }
     }
 }
