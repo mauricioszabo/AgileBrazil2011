0519fce5868c18e72fb118480b2afa2ca65e9c13
Maurício Szabo <mauricio.szabo@gmail.com>
2011-06-22 00:39:52 -0300

    Teste com o Java. Wow...

---
+++ pequenas_coisas/XMLParsingTest.java
@@ -0,0 +1,57 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.hamcrest.*;
+import static org.hamcrest.CoreMatchers.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import java.io.*;
+
+public class XMLParsingTest {
+    @Test public void getTheCorrectNumberOfPeopleOnXML() throws Exception {
+        DocumentBuilderFactory docBuilderF = DocumentBuilderFactory.newInstance();
+        docBuilderF.setNamespaceAware(false);
+        DocumentBuilder docBuilder = docBuilderF.newDocumentBuilder();
+        Document doc = docBuilder.parse(new ByteArrayInputStream(xml().getBytes()));
+        assertThat(doc.getElementsByTagName("person").getLength(), is(2));
+    }
+
+    @Test public void getTheCorrectNumberOfPeople() throws Exception {
+        assertThat(xml(), have(2).tags("person"));
+    }
+
+    private TagMatcher have(int count) {
+        return new TagMatcher(count);
+    }
+
+    class TagMatcher {
+        private int count;
+        public TagMatcher(int count) { this.count = count; }
+
+        public Matcher<String> tags(final String tagName) {
+            return new BaseMatcher<String>() {
+                public void describeTo(Description d) { 
+                    d.appendText("to have " + count + " <" + tagName + "> tags"); 
+                }
+
+                public boolean matches(Object xmlObj) {
+                    try {
+                        String xml = (String) xmlObj;
+                        DocumentBuilderFactory docBuilderF = DocumentBuilderFactory.newInstance();
+                        docBuilderF.setNamespaceAware(false);
+                        DocumentBuilder docBuilder = docBuilderF.newDocumentBuilder();
+                        Document doc = docBuilder.parse(new ByteArrayInputStream(xml.getBytes()));
+                        return doc.getElementsByTagName(tagName).getLength() == count;
+                    } catch(Exception e) {
+                        return false;
+                    }
+                }
+            };
+        }
+
+    }
+
+    private String xml() {
+      return "<people><person>Ariovaldo Steil</person>" +
+        "<person>Ariovaldo Gumercindo</person></people>";
+    }
+}
